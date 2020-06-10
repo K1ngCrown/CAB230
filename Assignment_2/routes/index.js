fe3cb94e409328ae8f03b3ef5df3a199fd1df9f5
K1ngCrown <shimarkalinsolorin@gmail.com>
2020-06-10 23:15:00 +1000

    server side

--- Assignment_2/routes/index.js
+++ Assignment_2/routes/index.js
@@ -6,25 +6,5 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/*
-router.post("/api/update", function (req, res) {
-  if (!req.body || !req.body || !req.body) {
-    res.status(400).json({ Message: "Error" })
-    return
-}
-
-const filter = {
-  Name: req.body.City,
-  CountryCode: req.body.CountryCode
-}
-const pop = { Population: req.body}
-
-
-req.body  
-  .from("city")
-  .where
-
-})
-*/
 
 module.exports = router;
--- Assignment_2/routes/stocks.js
+++ Assignment_2/routes/stocks.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 var mysql = require('mysql');
+var jwt = require('jsonwebtoken');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -35,7 +36,6 @@ router.get('/symbols', (req, res) => {
 
 });
 
-
 router.get('/:symbols', (req, res) => {
     const { from, to } = req.query;
     if (from != undefined | to != undefined) {
@@ -64,6 +64,7 @@ router.get('/:symbols', (req, res) => {
 
 
 const authorize = (req, res, next) => {
+    console.log("authorise");
     const authorization = req.headers.authorization
     let token = null;
 
@@ -71,64 +72,66 @@ const authorize = (req, res, next) => {
     if (authorization && authorization.split(" ").length === 2) {
         token = authorization.split(" ")[1]
         console.log("Token: ", token)
-        
     } else {
-        console.log("Unauthorized user")
+        res.status(403).json({ error: true, message: "Authorization header not found" }) 
         return;
     }
 
     try {
+        console.log("In try block");
+        const secretKey = "secret key";
         const decoded = jwt.verify(token, secretKey)
-
+        
         if (decoded.exp < Date.now()) {
             console.log("Token has expired")
-            
-            return
+            return;
         }
-
         next()
-    } catch (e) {
-        console.log("Token is not valid: ", err)
+    } catch (error) {
+        console.log("In catch block")
+        console.log("Token is not valid: ", error)
         return;
     }
 }
 
-router.post("/stocks/authed:symbols", authorize, function (req, res) {
+router.get("/authed/:symbols", authorize, function (req, res) {
     const { from, to } = req.query;
-    if (from != undefined | to != undefined) {
-        res.status(400).json({ error: true, message: "Bad Request" })
+    const { begin, until } = req.query;
+    if (begin != undefined || until != undefined) {
+        res.status(400).json({ error: true, message: "Parameters allowed are 'from' and 'to', example: /stocks/authed/AAL?from=2020-03-15" })
         return;
-    }
+    }  
 
     const stockSymbol = req.params.symbols;
 
-    req.db.from('stocks').distinct('*').where("symbol", stockSymbol)
+    req.db.from('stocks').select('*').where("symbol", stockSymbol).modify((query) => {
+        if (from) {
+            query.where('timestamp', ">=", new Date(from))
+        }
+        if (to){
+            query.where('timestamp', "<=", new Date(to))
+        }
+        if(!from && !to){
+            query.limit(1); 
+        }
+    }) 
         .then((rows) => {
-            if (rows.length !== 0) {
+            if (rows.length === 0) {
+                res.status(404).json({ error: true, message: "No entries available for query symbol for supplied date range" })
+            } else if (rows.length === 1) {
                 res.status(200).json(rows[0])
             } else {
-                res.status(404).json({ error: true, message: "No entry for symbol in stocks database" })
+                res.status(200).json(rows) 
             }
-
         })
         .catch((err) => {
             console.log(err);
             res.json({ error: true, message: "Error in MySQL query" })
         })
-})
-
-
-/*
-//Stock end point authorised
-router.get('/authed:stock', (req, res) => {
-    const stockSymbol = req.params.stock;
-    if (stockSymbol) {
-        return res.status(200).send({
-            success: true,
-            stock: stockSymbol
-        });
-    }
-    return res.status(500).send({ success: false });
 });
-*/
+
+
+
+
+
 module.exports = router;
